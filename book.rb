0f47d37ca5423837ca76a2c8ba06f35e649cc0fa
Leianne Louis <leilouis112@gmail.com>
2019-08-13 15:10:40 -0400

    added book.rb

---
+++ book.rb
@@ -0,0 +1,12 @@
+class Book
+    attr_reader :title :author
+    attr_writer :finished
+    attr_accessor :count
+    def initialize(title, author)
+        @title = title
+        @author = author
+        @finished = false
+        @count = 3
+    end
+end
+    
